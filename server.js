848f57b302098732d3033cfa75a9cd1d7810ee70
Lu Summers <lusummers@users.noreply.github.com>
2022-05-22 00:45:22 -0400

    update

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const {animals} = require("./data/animals");
 
-
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     // Note that we save the animalsArray as filteredResults here:
@@ -42,14 +45,22 @@ function filterByQuery(query, animalsArray) {
     // return the filtered results:
     return filteredResults;
   }
-
-app.get('/api/animals', (req, res) => {
+  app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
-    results = filterByQuery(req.query, results);
-  }
+      results = filterByQuery(req.query, results);
+    }
     res.json(results);
-});
+  });
+  
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
+  });
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
